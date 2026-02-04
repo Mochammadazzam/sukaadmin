b0805f46fdcb71ea650fdd24ed2868680aa88417
Mochammadazzam <Mochammadazzamarbiafifi@gmail.com>
2026-02-04 20:28:31 +0700

    Rename Main class to MainActivity and update imports

--- app/src/main/java/com/yoursite/app/Main.java
+++ app/src/main/java/com/yoursite/app/Main.java
@@ -1,157 +1,133 @@
-package com.yoursite.app;
+package com.sukaadmin.app; // DISESUAIKAN dari com.yoursite.app
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnLongClickListener;
 import android.webkit.CookieManager;
-import android.webkit.JsResult;
-import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class Main extends Activity {
+// Nama class diganti ke MainActivity agar standar dengan Manifest
+public class MainActivity extends Activity {
 
-	private WebView webView;
-	private String url;
-	
-	@Override
-	protected void onStop() {
-		// TODO Auto-generated method stub
-		super.onStop();
-		saveCookies();
-	}
+    private WebView webView;
+    private String url;
+    
+    @Override
+    protected void onStop() {
+        super.onStop();
+        saveCookies();
+    }
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		//setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-		url = "file:///android_asset/index.html";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Pastikan di folder res/layout ada file activity_main.xml
+        setContentView(R.layout.activity_main);
+        
+        url = "file:///android_asset/index.html";
 
-		// Create reference to UI elements
-		webView = (WebView) findViewById(R.id.webview_compontent);
+        // ID harus sama dengan yang ada di layout XML kamu
+        webView = (WebView) findViewById(R.id.webview_compontent);
 
-		webView.setOnLongClickListener(new OnLongClickListener() {
-			@Override
-			public boolean onLongClick(View v) {
-				return true;
-			}
-		});
-		webView.setLongClickable(false);
+        webView.setOnLongClickListener(new OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                return true;
+            }
+        });
+        webView.setLongClickable(false);
 
-		// workaround so that the default browser doesn't take over
-		webView.setWebViewClient(new MyWebViewClient());
-		webView.getSettings().setJavaScriptEnabled(true);
-		webView.getSettings().setDomStorageEnabled(true);
-		webView.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
+        webView.setWebViewClient(new MyWebViewClient());
+        WebSettings settings = webView.getSettings();
+        
+        // Setting wajib agar HTML/Firebase jalan
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setCacheMode(WebSettings.LOAD_DEFAULT);
 
-		CookieManager.setAcceptFileSchemeCookies(true);
+        CookieManager.getInstance().setAcceptCookie(true);
+        CookieManager.setAcceptFileSchemeCookies(true);
 
-		restoreCookies();
-		if (savedInstanceState == null) {
-			webView.loadUrl(url);
-		}
+        restoreCookies();
+        if (savedInstanceState == null) {
+            webView.loadUrl(url);
+        }
 
-		/* Periodically saves cookies to variables */
-		new Timer().scheduleAtFixedRate(new TimerTask() {
-			@Override
-			public void run() {
-				saveCookies();
-			}
-		}, 0, 10000);
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                saveCookies();
+            }
+        }, 0, 10000);
+    }
 
-	}
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if(webView != null) webView.saveState(outState);
+        saveCookies();
+    }
 
-	@Override
-	protected void onSaveInstanceState(Bundle outState) {
-		super.onSaveInstanceState(outState);
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if(webView != null) webView.restoreState(savedInstanceState);
+    }
 
-		// Save the state of the WebView
-		webView.saveState(outState);
-		saveCookies();
-	}
+    private void saveCookies() {
+        String cookies = CookieManager.getInstance().getCookie(url);
+        SharedPreferences sp = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        SharedPreferences.Editor prefsEditor = sp.edit();
+        prefsEditor.putString("cookies", cookies);
+        prefsEditor.apply(); // pakai apply lebih efisien daripada commit
+    }
+    
+    private void restoreCookies() {
+        SharedPreferences sp = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        String cookies = sp.getString("cookies", "");
+        CookieManager.getInstance().setCookie(url, cookies);
+    }
 
-	@Override
-	protected void onRestoreInstanceState(Bundle savedInstanceState) {
-		super.onRestoreInstanceState(savedInstanceState);
-
-		// Restore the state of the WebView
-		webView.restoreState(savedInstanceState);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		// getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-	
-	/* method to save cookies in app memory */
-	private void saveCookies() {
-		String cookies = CookieManager.getInstance().getCookie(url);
-		// Saving cookies to MyPrefs
-		SharedPreferences sp=getSharedPreferences("MyPrefs",MODE_PRIVATE);
-		SharedPreferences.Editor prefsEditor = sp.edit();
-		prefsEditor.putString("cookies", cookies);
-		prefsEditor.commit();
-	}
-	
-	/* restoring cookies from app memory */
-	private void restoreCookies() {
-		SharedPreferences sp=getSharedPreferences("MyPrefs",MODE_PRIVATE);
-		String cookies = sp.getString("cookies", "");
-		CookieManager.getInstance().setCookie(url,cookies);
-	}
     @Override
     public void onBackPressed() {
-        if(webView!= null && webView.canGoBack()) {
-			webView.goBack();// if there is previous page open it
-			saveCookies();
-		}
-        else {
-			saveCookies();
-			super.onBackPressed();//if there is no previous page, close app
-		}
+        if(webView != null && webView.canGoBack()) {
+            webView.goBack();
+            saveCookies();
+        } else {
+            saveCookies();
+            super.onBackPressed();
+        }
     }
 
     private class MyWebViewClient extends WebViewClient {
-		@Override
-		public boolean shouldOverrideUrlLoading(WebView view, String url) {
-			if (url.startsWith("mailto:")) {
-				url = url.replaceFirst("mailto:", "");
-				url = url.trim();
-				Intent i = new Intent(Intent.ACTION_SEND);
-				i.setType("plain/text").putExtra(Intent.EXTRA_EMAIL,
-						new String[] { url });
-				startActivity(i);
-				return true;
-			}
-			view.loadUrl(url);
-			return true;
-		}
-
-	}
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            // Logika untuk WhatsApp Link agar tidak error di WebView
+            if (url.startsWith("https://wa.me/") || url.startsWith("whatsapp:")) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(android.net.Uri.parse(url));
+                startActivity(intent);
+                return true;
+            }
+            
+            if (url.startsWith("mailto:")) {
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i.setType("plain/text").putExtra(Intent.EXTRA_EMAIL, new String[] { url.replace("mailto:", "") });
+                startActivity(i);
+                return true;
+            }
+            view.loadUrl(url);
+            return true;
+        }
+    }
 }
